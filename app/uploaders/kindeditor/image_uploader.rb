5b9495d795189ad9848ef43ae94a0c3a5b6133d8
hosanna <ohosanna@gmail.com>
2012-11-04 20:20:50 +0800

    读取自定义图片上传设置

--- app/uploaders/kindeditor/image_uploader.rb
+++ app/uploaders/kindeditor/image_uploader.rb
@@ -2,6 +2,16 @@
 
 class Kindeditor::ImageUploader < Kindeditor::AssetUploader
   
+  CarrierWave.configure do |config|
+    config.storage = :upyun
+    config.upyun_username = "hosanna"
+    config.upyun_password = 'hosanna92304150'
+    config.upyun_bucket = "72cy-dev"
+    config.upyun_bucket_domain = "72cy-dev.b0.upaiyun.com"
+  end
+
+  storage :upyun
+
   def extension_white_list
     EXT_NAMES[:image]
   end
--- lib/rails_kindeditor/helper.rb
+++ lib/rails_kindeditor/helper.rb
@@ -27,16 +27,17 @@ module RailsKindeditor
     end
 
     def get_options(options)
-     # options.delete(:uploadJson)
-     # options.delete(:fileManagerJson)
+      #options.delete(:uploadJson)
+      options.delete(:fileManagerJson)
       options.reverse_merge!(:width => '100%')
       options.reverse_merge!(:height => 300)
       options.reverse_merge!(:allowFileManager => true)
       options.reverse_merge!(:uploadJson => '/kindeditor/upload')
-      options.reverse_merge!(:fileManagerJson => '/kindeditor/filemanager')
+      #options.merge!(:uploadJson => '/kindeditor/upload')
+      options.merge!(:fileManagerJson => '/kindeditor/filemanager')
       if options[:simple_mode] == true
         options.delete(:simple_mode)
-        options.merge!(:items => %w{fontname fontsize | forecolor hilitecolor bold italic underline removeformat | justifyleft justifycenter justifyright insertorderedlist insertunorderedlist | emoticons image multiimage link})
+        options.merge!(:items => %w{fontname fontsize | forecolor hilitecolor bold italic underline removeformat | justifyleft justifycenter justifyright insertorderedlist insertunorderedlist | emoticons image link})
       end
       options
     end    
